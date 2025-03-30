c19d5ca6a8e753a23fb9609763833a8e281a09ae
insotheo <maxim.insotheo@gmail.com>
2025-03-30 16:25:37 +0400

    Hello, World

---
+++ HTTPNotepad/Net/Server.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Net;
+
+namespace HTTPNotepad.Net
+{
+    class Server : IDisposable
+    {
+        HttpListener listener;
+
+        public Server(string prefix)
+        {
+            listener = new HttpListener();
+            listener.Prefixes.Add(prefix);
+        }
+
+        public void Start() => listener.Start();
+
+        public ref HttpListener GetReference() => ref listener;
+
+        public void Dispose()
+        {
+            listener.Stop();
+            listener.Close();
+        }
+    }
+}
---
+++ HTTPNotepad/Program.cs
@@ -0,0 +1,33 @@
+﻿using HTTPNotepad.Net;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPNotepad
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using(Server server = new Server("http://localhost:5050/"))
+            {
+                server.Start();
+                while (true)
+                {
+                    HttpListenerContext ctx = await server.GetReference().GetContextAsync();
+                    HttpListenerRequest request = ctx.Request;
+                    HttpListenerResponse response = ctx.Response;
+
+                    string respString = "<html><body><h1>Hello, World!</h1></body></html>";
+                    byte[] buffer = Encoding.UTF8.GetBytes(respString);
+                    response.ContentLength64 = buffer.Length;
+                    using(var output = response.OutputStream)
+                    {
+                        await output.WriteAsync(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+    }
+}
